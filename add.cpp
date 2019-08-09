5d8dfcda79a067e0cd3454b1222503b90838911a
swasti <swastishreya.mishra@iiitb.org>
2019-08-09 16:21:23 +0530

    Added functionality to multiply numbers

--- add.cpp
+++ add.cpp
@@ -3,11 +3,27 @@
 using namespace std;
 
 int main(){
-    int a, b;
+    float a, b;
+    int choice;
 
-    cin >> a >> b;
-
-    cout << a+b;
+    cout << "1.Add two numbers\n2.Multiply two numbers\n3.Exit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    while(choice != 3){
+        cout << "Enter number 1: ";
+        cin >> a;
+        cout << "Enter number 2: ";
+        cin >> b;
+        if(choice == 1){
+            cout << "Result: " << a+b << endl; 
+        }
+        else if(choice == 2){
+            cout << "Result: " << a*b << endl; 
+        }
+        cout << "1.Add two numbers\n2.Multiply two numbers\n3.Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+    }
 
     return 0;
 }
